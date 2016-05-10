9baebf5f21829e748c6adea1fce525929dbade2b
liuhongzhe <wy_liuhongzhe@163.com>
2016-05-10 17:26:15 +0800

    first commit

---
+++ IOSDDU/IOSDDU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSDDU
+//
+//  Created by liuhongzhe on 16/5/10.
+//  Copyright © 2016年 yzbzz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
